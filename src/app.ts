97fb484b92acce27bc6c29773e63650d9bab1606
Julian Garbate <juliangarbate@gmail.com>
2026-06-29 22:26:52 -0300

    vercel x8

--- src/app.ts
+++ src/app.ts
@@ -28,6 +28,28 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+	const allowedOrigins = new Set([
+		"http://localhost:3000",
+		"https://proyecto-epof.vercel.app",
+		process.env.FRONTEND_URL,
+	].filter((value): value is string => Boolean(value)));
+
+	const origin = req.headers.origin;
+	if (origin && allowedOrigins.has(origin)) {
+		res.setHeader("Access-Control-Allow-Origin", origin);
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+		res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+	}
+
+	if (req.method === "OPTIONS") {
+		res.sendStatus(200);
+		return;
+	}
+
+	next();
+});
 app.use(express.json());
 
 app.use("/api", routes);
